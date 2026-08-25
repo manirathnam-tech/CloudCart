194d69632faa25a6df729dbc9a5dc6c31442494b
Manirathnam_IRL <manirathnam.irl@gmail.com>
2026-08-25 12:39:23 +0100

    Define VPC variables in variables.tf
    Added variables for AWS VPC configuration including region, CIDR blocks, availability zones, and environment.

---
+++ infra/terraform/modules/vpc/variables.tf
@@ -0,0 +1,36 @@
+variable "aws_region" {
+  description = "The AWS region to deploy resources in"
+  type        = string
+  default     = "eu-west-1"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+}
+
+variable "availability_zones" {
+  description = "The availability zones to deploy resources in"
+  type        = list(string)
+  default     = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
+}
+
+variable "public_subnet_cidrs" {
+  description = "The CIDR blocks for the public subnets"
+  type        = list(string)
+}
+
+variable "private_subnet_cidrs" {
+  description = "The CIDR blocks for the private subnets"
+  type        = list(string)
+}
+
+variable "data_subnet_cidrs" {
+  description = "The CIDR blocks for the data subnets"
+  type        = list(string)
+}
+
+variable "env" {
+  description = "The environment for the resources (e.g., dev, prod)"
+  type        = string
+}
