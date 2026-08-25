76164d4ab9ca7d0fdf49b5d0390d4b412ffc29d3
Manirathnam_IRL <manirathnam.irl@gmail.com>
2026-08-25 13:10:00 +0100

    Remove aws_region variable from VPC module
    Removed aws_region variable definition from VPC module.

--- infra/terraform/modules/vpc/variables.tf
+++ infra/terraform/modules/vpc/variables.tf
@@ -1,9 +1,3 @@
-variable "aws_region" {
-  description = "The AWS region to deploy resources in"
-  type        = string
-  default     = "eu-west-1"
-}
-
 variable "vpc_cidr" {
   description = "CIDR block for the VPC"
   type        = string
