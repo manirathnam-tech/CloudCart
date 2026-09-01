3828ce7685490e0e66d2a5a69405ac39850fb098
Manirathnam_IRL <manirathnam.irl@gmail.com>
2026-09-01 15:31:24 +0100

    Add outputs for NAT Gateway and route tables
    Added outputs for NAT Gateway and route tables in dev environment.

--- infra/terraform/environments/dev/outputs.tf
+++ infra/terraform/environments/dev/outputs.tf
@@ -27,3 +27,23 @@ output "data_subnet_ids" {
   value       = module.vpc.data_subnet_ids
   description = "The IDs of the dev data subnets"
 }
+
+output "nat_gateway_ids" {
+  value       = [aws_nat_gateway.CloudCart_nat_gw.id]
+  description = "The IDs of the NAT Gateway(s)"
+}
+
+output "public_route_table_id" {
+  value       = aws_route_table.CloudCart_public_rt.id
+  description = "The ID of the public route table"
+}
+
+output "private_route_table_id" {
+  value       = aws_route_table.CloudCart_private_rt.id
+  description = "The ID of the private route table"
+}
+
+output "data_route_table_id" {
+  value       = aws_route_table.CloudCart_data_rt.id
+  description = "The ID of the data route table"
+}
