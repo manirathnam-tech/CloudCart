f9949b34eb9488f033587d0300bb90e5ea34d066
Manirathnam_IRL <manirathnam.irl@gmail.com>
2026-09-11 13:11:07 +0100

    Add variables for EKS module configuration

---
+++ infra/terraform/modules/eks/variables.tf
@@ -0,0 +1,15 @@
+variable "env" {
+  type = string
+}
+
+variable "vpc_id" {
+  type = string
+}
+
+variable "private_subnet_ids" {
+  type = list(string)
+}
+
+variable "eks_public_access_cidrs" {
+  type = list(string)
+}
