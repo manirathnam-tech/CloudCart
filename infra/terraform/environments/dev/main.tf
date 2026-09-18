200d80c0c436ad9b84c18a37bc2b3d55926650c1
Manirathnam_IRL <manirathnam.irl@gmail.com>
2026-09-18 20:26:31 +0100

    Confirm module wiring for eks module

--- infra/terraform/environments/dev/main.tf
+++ infra/terraform/environments/dev/main.tf
@@ -9,10 +9,10 @@ module "vpc" {
 }
 
 module "security_groups" {
-source = "../../modules/security-groups"
+  source = "../../modules/security-groups"
 
-vpc_id = module.vpc.vpc_id
-env = var.env 
+  vpc_id = module.vpc.vpc_id
+  env    = var.env
 }
 
 module "eks" {
